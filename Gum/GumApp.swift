0033b90b711f3461ddd73699e1b9f36fca389620
KRWD <krwd@KRWDui-MacBookAir.local>
2023-10-26 00:26:25 +0900

    gitIgnore 추가

---
+++ Gum/GumApp.swift
@@ -0,0 +1,17 @@
+//
+//  GumApp.swift
+//  Gum
+//
+//  Created by KRWD on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
